466293d0930817c3526492d6ed83356e32a97d9b
Christopher Knight Johnson <ckj@Jackalope.local>
2017-12-05 21:41:11 -0800

    add sass structure, redo breakpoints via mobile first

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-  $("header i.fa.fa-bars").click(function(){
-    $("nav").slideToggle();
-  });
-  $(window).resize(function(){
-    if ($(window).width() > 660) {
-      $("nav").show();
-    } else {
-      $("nav").hide();
-    }
-  });
-});
+// $(document).ready(function(){
+//   $("header i.fa.fa-bars").click(function(){
+//     $("nav").slideToggle();
+//   });
+//   $(window).resize(function(){
+//     if ($(window).width() > 660) {
+//       $("nav").show();
+//     } else {
+//       $("nav").hide();
+//     }
+//   });
+// });
